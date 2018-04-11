f39e5a032232059b798fc40f5bfd5dea84f714f9
chandu2202 <chandukorlapati@gmail.com>
2018-04-11 00:18:46 -0500

    Initial Commit

---
+++ src/com/SampleReact/JavaSmpl.java
@@ -0,0 +1,10 @@
+package com.SampleReact;
+
+public class JavaSmpl {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String a = str.substring(4);
+
+        System.out.println(a+a);
+    }
+}
